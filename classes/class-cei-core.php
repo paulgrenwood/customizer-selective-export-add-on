7d4ec261261c04207fbfe7f2725c0324f2ab24a5
Paul Grenwood <hello@thegrenwoods.com>
2020-02-05 11:56:25 -0800

    Removing Comments from Opt-in Array

--- classes/class-cei-core.php
+++ classes/class-cei-core.php
@@ -70,263 +70,139 @@ final class CEI_Core {
 	);
 	
 	static private $keys_optin__typography = array(
-		//=== BODY FONT ===
-		//• Font Size
 		'fl-body-font-size',
 		'fl-body-font-size_medium',
 		'fl-body-font-size_mobile',
-		
-		//• Line Height
 		'fl-body-line-height',
 		'fl-body-line-height_medium',
 		'fl-body-line-height_mobile',
-		
-		//• Font Family
 		'fl-body-font-family',
-		
-		//• Font Weight
 		'fl-body-font-weight',
-		
-		//• Margin Bottom
 		'b42s__body_font__margin_bottom',
 		'b42s__body_font__margin_bottom_medium',
 		'b42s__body_font__margin_bottom_mobile',
-		
-		//=== INTRO TEXT ===
-		//• Font Size
 		'b42s__intro_text__font_size',
 		'b42s__intro_text__font_size_medium',
 		'b42s__intro_text__font_size_mobile',
-		
-		//• Line Height
 		'b42s__intro_text__line_height',
 		'b42s__intro_text__line_height_medium',
 		'b42s__intro_text__line_height_mobile'
-		
-		//• Margin Bottom
 		'b42s__intro_text__margin_bottom',
 		'b42s__intro_text__margin_bottom_medium',
 		'b42s__intro_text__margin_bottom_mobile',
-		
-		//== INTRO TEXT ALT ===
-		//• Font Size
 		'b42s__intro_text_alt__font_size',
 		'b42s__intro_text_alt__font_size_medium',
 		'b42s__intro_text_alt__font_size_mobile',
-		
-		//• Line Height
 		'b42s__intro_text_alt__line_height',
 		'b42s__intro_text_alt__line_height_medium',
 		'b42s__intro_text_alt__line_height_mobile',
-		
-		//• Margin Bottom
 		'b42s__intro_text_alt__margin_bottom',
 		'b42s__intro_text_alt__margin_bottom_medium',
 		'b42s__intro_text_alt__margin_bottom_mobile',
-		
-		
-		//=== SMALL TEXT ===
-		//• Font Size
 		'b42s__small_text__font_size',
 		'b42s__small_text__font_size_medium',
 		'b42s__small_text__font_size_mobile',
-		
-		//• Line Height
 		'b42s__small_text__line_height',
 		'b42s__small_text__line_height_medium',
 		'b42s__small_text__line_height_mobile',
-		
-		//• Margin Bottom
 		'b42s__small_text__margin_bottom',
 		'b42s__small_text__margin_bottom_medium',
 		'b42s__small_text__margin_bottom_mobile',
-		
-		
-		//=== HEADER NAV ===
-		//• General
 		'b42s__header_nav__font_format',
 		'b42s__header_nav__font_weight',
-		
-		//• Font Size
 		'b42s__header_nav__font_size',
 		'b42s__header_nav__font_size_medium',
 		'b42s__header_nav__font_size_mobile',
-		
-		//• Line Height
 		'b42s__header_nav__line_height',
 		'b42s__header_nav__line_height_medium',
 		'b42s__header_nav__line_height_mobile',
-		
-		//• Letter Spacing
 		'b42s__header_nav__letter_spacing',
 		'b42s__header_nav__letter_spacing_medium',
 		'b42s__header_nav__letter_spacing_mobile'
-		
-		
-		//=== QUOTE ===
-		//• Font Size
 		'b42s__blockquote__font_size',
 		'b42s__blockquote__font_size_medium',
 		'b42s__blockquote__font_size_mobile',
-		
-		//• Line Height
 		'b42s__blockquote__line_height',
 		'b42s__blockquote__line_height_medium',
 		'b42s__blockquote__line_height_mobile',
-		
-		//• Margin Bottom
 		'b42s__blockquote__margin_bottom',
 		'b42s__blockquote__margin_bottom_medium',
 		'b42s__blockquote__margin_bottom_mobile',
-		
-		
-		//=== NAV ===
 		'fl-nav-font-family',
 		'fl-nav-font-weight',
 		'fl-nav-font-format',
 		'fl-nav-font-size',
-		
-		
-		//=== HEADINGS ===
-		//• Title
 		'fl-heading-style',
 		'fl-title-font-family',
 		'fl-title-font-weight',
 		'fl-title-font-format',
-		
-		//• Headings
 		'fl-heading-font-family',
 		'fl-heading-font-weight',
 		'fl-heading-font-format',
-		
-		//=== H1 ===
-		//• Font Size
 		'fl-h1-font-size',
 		'fl-h1-font-size_medium',
 		'fl-h1-font-size_mobile',
-		
-		//• Line Height
 		'fl-h1-line-height',
 		'fl-h1-line-height_medium',
 		'fl-h1-line-height_mobile',
-		
-		//• Letter Spacing
 		'fl-h1-letter-spacing',
 		'fl-h1-letter-spacing_medium',
 		'fl-h1-letter-spacing_mobile',
-		
-		//=== H2 ===
-		//• Font Size
 		'fl-h2-font-size',
 		'fl-h2-font-size_medium',
 		'fl-h2-font-size_mobile',
-		
-		//• Line Height
 		'fl-h2-line-height',
 		'fl-h2-line-height_medium',
 		'fl-h2-line-height_mobile',
-		
-		//• Letter Spacing
 		'fl-h2-letter-spacing',
 		'fl-h2-letter-spacing_medium',
 		'fl-h2-letter-spacing_mobile',
-		
-		//• Font Weight
 		'b42s__h2__font_weight',
-		
-		//• Font Format
 		'b42s__h2__font_format',
-		
-		//=== H3 ===
-		//• Font Size
 		'fl-h3-font-size',
 		'fl-h3-font-size_medium',
 		'fl-h3-font-size_mobile',
-		
-		//• Line Height
 		'fl-h3-line-height',
 		'fl-h3-line-height_medium',
 		'fl-h3-line-height_mobile',
-		
-		//• Letter Spacing
 		'fl-h3-letter-spacing',		
 		'fl-h3-letter-spacing_medium',
 		'fl-h3-letter-spacing_mobile',
-		
-		//• Font Weight
 		'b42s__h3__font_weight',
-		
-		//• Font Format
 		'b42s__h4__font_format',
-		
-		//=== H4 ===
-		//• Font Size
 		'fl-h4-font-size',
 		'fl-h4-font-size_medium',
 		'fl-h4-font-size_mobile',
-		
-		//• Line Height
 		'fl-h4-line-height',
 		'fl-h4-line-height_medium',
 		'fl-h4-line-height_mobile',
-		
-		//• Letter Spacing
 		'fl-h4-letter-spacing',
 		'fl-h4-letter-spacing_medium',
 		'fl-h4-letter-spacing_mobile',
-		
-		//• Font Weight
 		'b42s__h4__font_weight',
-		
-		//• Font Format
 		'b42s__h4__font_format',
-		
-		//=== H5 ===
-		//• Font Size
 		'fl-h5-font-size',
 		'fl-h5-font-size_medium',
 		'fl-h5-font-size_mobile',
-		
-		//• Line Height
 		'fl-h5-line-height',
 		'fl-h5-line-height_medium',
 		'fl-h5-line-height_mobile',
-		
-		//• Letter Spacing
 		'fl-h5-letter-spacing',
 		'fl-h5-letter-spacing_medium',
 		'fl-h5-letter-spacing_mobile',
-		
-		//• Font Weight
 		'b42s__h5__font_weight',
-		
-		//• Font Format
 		'b42s__h5__font_format',
-		
-		//=== H6 ===
-		//• Font Size
 		'fl-h6-font-size',
 		'fl-h6-font-size_medium',
 		'fl-h6-font-size_mobile',
-		
-		//• Line Height
 		'fl-h6-line-height',
 		'fl-h6-line-height_medium',
 		'fl-h6-line-height_mobile',
-		
-		//• Letter Spacing
 		'fl-h6-letter-spacing',
 		'fl-h6-letter-spacing_medium',
 		'fl-h6-letter-spacing_mobile',
-		
-		//• Font Weight
 		'b42s__h6__font_weight',
-		
-		//• Font Format
 		'b42s__h6__font_format',
-		
-		
-		//=== BUTTONS ===
 		'fl-button-font-family',
 		'fl-button-font-weight',
 		'fl-button-font-size',
