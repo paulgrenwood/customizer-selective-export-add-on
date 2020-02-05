261188691244305d34881afc3edf524f835340f1
Paul Grenwood <hello@thegrenwoods.com>
2020-02-05 09:23:54 -0800

    Initialization

---
+++ customizer-selective-export-add-on.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Customizer Selective Export Add-on
+ * Plugin URI: https://github.com
+ * Description: Selective Export Add-on for Customizer Export/Import Plugin: http://www.wpbeaverbuilder.com/wordpress-customizer-export-import-plugin/?utm_source=external&utm_medium=customizer-export&utm_campaign=plugins-page
+ * Version: 0.0.1
+ * Author: Paul Grenwood
+ * Author URI: https://thegrenwoods.com
+ * License: GNU General Public License v2.0
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: customizer-selective-export
